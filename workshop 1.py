31b91deebf05f1da2e277939153f3068ce60fced
Veno <govanygozief24@gmail.com>
2025-02-18 11:04:31 +0200

    first commit

---
+++ workshop 1.py
@@ -0,0 +1,107 @@
+# q1
+# x= 5
+# y=6
+# print(x+y)
+# print(f"{x}")
+# print(str(x))
+# print(type(y))
+
+
+# # q2
+# x=input("Enter the first number: ") 
+# y=input("Enter the second number: ")
+# print('the result is',x+y)
+# print('the result is',x-y)
+# print('the result is',x*y)
+# print('the result is',x/y)
+
+
+# q3
+# x = int(input("Enter your rate: "))
+# if x == 5 :
+#     print("your rate is excellent")
+# elif x == 4 :
+#     print("your rate is very good")
+# elif x == 3 :
+#     print("your rate is good")
+# elif x == 2 :
+#     print("your rate is bad")
+# elif x == 1 :
+#     print("your rate is very bad")
+# else:
+#     print("your rate between 1 and 5")
+
+
+# q4
+# x = int(input("Enter your number: "))
+# for i in range(1,6):
+#     i = x*i 
+# print(i)
+
+
+# import random
+# x = int(input("Enter your number: "))
+# random_number = random.randint(1,10)
+# y = x*random_number
+# print(y)
+
+
+# q5
+# x= int(input("Enter your number: "))
+# sum=0
+# for i in range(1,x+1):
+#     if i %2==0:
+#         sum+= i
+# print(f"the sum of even numbers is {sum}")
+
+
+#q6 
+# x= int(input("Enter your number: "))
+# for i in range(1,11):
+#     print(x-i)
+
+
+# q7
+# x = "hello"
+# y = input("Enter your password: ") 
+# if y == x:
+#     print("Access Greated")
+# else:
+#     print("wrong password")
+#     y = input("Enter your password: ")
+#     print("Access Greated")
+
+
+# q8
+# number = {10,20,30,40,50}
+# total = sum(number)
+# print(f"the sum of the numbers is {total}")
+
+
+# q9
+# number = {10,20,30,40,50}
+# max = max(number)
+# min = min(number)
+# print(f"the min number is {min}")
+# print(f"the max number is {max}") 
+
+
+# q10
+# student = {"name":"sara","age":25,"gpa":3.5}
+# print(f"the name is {student['name']}")
+# print(f"the age is {student['age']}")
+# print(f"the gpa is {student['gpa']}")
+
+
+# q11
+# student = {"name":"sara","age":25,"gpa":3.5}
+# print(f"name: {student['name']}")
+# print(f"age: {student['age']}")
+# print(f"gpa: {student['gpa']}")
+
+
+# q12
+# car  ={"brand":"ford","model":"mustang","year":1964}
+# for key,value in car.items():
+#     print(f"{key} : {value}")
+
